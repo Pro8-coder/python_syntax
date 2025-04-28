508529bf2e780ae82202ad9f56565e461b6380b8
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2025-04-28 14:53:53 +0300

    Init

--- 4.2 Вложенные условия и множественный выбор/4.py
+++ 4.2 Вложенные условия и множественный выбор/4.py
@@ -1,47 +1,30 @@
 """
-На вход программе подается натуральное число N, которое необходимо прочитать и
-сохранить через переменную. Используя строки из латинских букв ascii_lowercase
-и ascii_uppercase:
-
-from string import ascii_lowercase, ascii_uppercase
-chars = ascii_lowercase + ascii_uppercase
-объявите функцию-генератор с одним параметром max_size, которая бы возвращала
-случайно сформированные email-адреса с доменом mail.ru и длиной max_size = N
-символов. Например, при N=6 адрес может выглядеть так: SCrUZo@mail.ru
-
-Функция-генератор должна возвращать бесконечное число таких адресов, то есть,
-генерировать постоянно. Выведите первые пять сгенерированных email и выведите
-их в столбик (каждый с новой строки).
-
-Подсказка: для формирования случайного индекса для выбора символа из строки
-chars, используйте функцию randint модуля random:
-
-import random
-random.seed(1)
-indx = random.randint(0, len(chars)-1)
-
+На вход программе подается целое число: порядковый номер дня недели
+(1, 2, ..., 7). Необходимо прочитать это число и вывести на экран название дня
+недели:
+понедельник, вторник, среда, четверг, пятница, суббота, воскресенье
+Программу реализовать с использованием операторов if-elif.
 Sample Input:
 
-8
+2
 Sample Output:
 
-iKZWeqhF@mail.ru
-WCEPyYng@mail.ru
-FbyBMWXa@mail.ru
-SCrUZoLg@mail.ru
-ubbbPIay@mail.ru
+вторник
 """
-from random import choice, seed
-from string import ascii_lowercase, ascii_uppercase
-from typing import Iterator
 
-seed(1)
-chars = ascii_lowercase + ascii_uppercase
-
-
-def mail_gen(max_size: int) -> Iterator[str]:
-    yield "".join(choice(chars) for _ in range(max_size)) + "@mail.ru"
-
-
-mail_size = int(input())
-print(*[next(mail_gen(mail_size)) for _ in range(5)], sep='\n')
+day = int(input())
+if day == 1:
+    day = 'понедельник'
+elif day == 2:
+    day = 'вторник'
+elif day == 3:
+    day = 'среда'
+elif day == 4:
+    day = 'четверг'
+elif day == 5:
+    day = 'пятница'
+elif day == 6:
+    day = 'суббота'
+elif day == 7:
+    day = 'воскресенье'
+print(day)
--- 9.2 Функция-генератор. Оператор yield/4.py
+++ 9.2 Функция-генератор. Оператор yield/4.py
@@ -1,3 +1,47 @@
 """
+На вход программе подается натуральное число N, которое необходимо прочитать и
+сохранить через переменную. Используя строки из латинских букв ascii_lowercase
+и ascii_uppercase:
 
+from string import ascii_lowercase, ascii_uppercase
+chars = ascii_lowercase + ascii_uppercase
+объявите функцию-генератор с одним параметром max_size, которая бы возвращала
+случайно сформированные email-адреса с доменом mail.ru и длиной max_size = N
+символов. Например, при N=6 адрес может выглядеть так: SCrUZo@mail.ru
+
+Функция-генератор должна возвращать бесконечное число таких адресов, то есть,
+генерировать постоянно. Выведите первые пять сгенерированных email и выведите
+их в столбик (каждый с новой строки).
+
+Подсказка: для формирования случайного индекса для выбора символа из строки
+chars, используйте функцию randint модуля random:
+
+import random
+random.seed(1)
+indx = random.randint(0, len(chars)-1)
+
+Sample Input:
+
+8
+Sample Output:
+
+iKZWeqhF@mail.ru
+WCEPyYng@mail.ru
+FbyBMWXa@mail.ru
+SCrUZoLg@mail.ru
+ubbbPIay@mail.ru
 """
+from random import choice, seed
+from string import ascii_lowercase, ascii_uppercase
+from typing import Iterator
+
+seed(1)
+chars = ascii_lowercase + ascii_uppercase
+
+
+def mail_gen(max_size: int) -> Iterator[str]:
+    yield "".join(choice(chars) for _ in range(max_size)) + "@mail.ru"
+
+
+mail_size = int(input())
+print(*[next(mail_gen(mail_size)) for _ in range(5)], sep='\n')
