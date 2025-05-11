05e78d236938299ed128c6b405356049dde895f9
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2025-05-11 21:40:37 +0300

    Init

---
+++ 7.1 Что такое функции. Их объявление и вызов/1.py
@@ -0,0 +1,16 @@
+"""
+Объявите в программе функцию, которая не имеет параметров и просто выводит
+на экран следующую строку:
+
+It's my first function
+
+После объявления вызовите эту функцию.
+"""
+
+
+def first_function():
+    text = "It's my first function"
+    print(text)
+
+
+first_function()
